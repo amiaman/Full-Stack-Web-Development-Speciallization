b6cada1a2f94ed4d00e79cf887e4cfc131fad523
Aman Kumar Singh <amanami00@gmail.com>
2016-05-08 11:24:24 +0530

    Create maths.js

---
+++ Server-side-Development-with-NodeJS/Nodejs/maths.js
@@ -0,0 +1,21 @@
+
+exports.addition = function(x, y) {
+       return (x+y);
+}
+
+exports.substraction = function(x, y) {
+     return (x-y);
+}
+
+
+exports.squer = function(x){
+         return (x*x);
+}
+
+exports.multiplication = function(x, y) {
+        return (x*y);
+}
+
+exports.division = function(x, y)
+      return (x/y);
+}
