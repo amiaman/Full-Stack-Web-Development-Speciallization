406b0ecc20e4f0925882d90453ede7633ef531d8
Aman Kumar Singh <amanami00@gmail.com>
2016-05-08 11:37:48 +0530

    Create maths_solve.js

---
+++ Server-side-Development-with-NodeJS/Nodejs/maths_solve.js
@@ -0,0 +1,15 @@
+var math = require('./maths')
+
+function mathsSolve(a, b){
+  console.log("addition of " + a + "and" + b + "is" + math.addition(a, b));
+
+  console.log("substraction of " + a + "and" + b + "is" + math.substraction(a, b));
+
+  console.log("multiplication of " + a + "and" + b + "is" + math.multiplication(a, b));
+
+  
+}
+
+mathsSolve(4, 2);
+mathsSolve(30, 5);
+mathsSolve(18,9);
